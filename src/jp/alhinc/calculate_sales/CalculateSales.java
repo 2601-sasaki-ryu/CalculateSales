a057a6b67830f685e296a2bbd8f1645d908f47d7
sasaki_ryu <sasaki.ryu@alhinc.jp>
2026-03-09 17:04:16 +0900

    3/9 作業分commit

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CalculateSales {
@@ -37,7 +39,55 @@ public class CalculateSales {
 		}
 
 		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+		File[] files = new File(args[0]).listFiles(); 
+		
+		//先にファイルの情報を格納する List(ArrayList) 宣言します。
+		List<File> rcdFiles = new ArrayList<>(); 
+		//ここですべてのファイルから数字8桁のrcdファイルを取り出したい
+		for(int i = 0; i < files.length ; i++) { 
+				if(files[i].getName().matches("^[0-9]{8}.rcd$")) { 
+			            //売上ファイルの条件に当てはまったものだけ、List(ArrayList) に追加します。
+					rcdFiles.add(files[i]); 		
+				} 
+		}
+		//rcdFilesに複数の売上ファイルの情報を格納しているので、その数だけ繰り返します。
+		for(int i = 0; i < rcdFiles.size(); i++) {
 
+			//支店定義ファイル読み込み(readFileメソッド)を参考に売上ファイルの中身を読み込みます。
+			BufferedReader br = null;
+			try {
+				FileReader fr = new FileReader(rcdFiles.get(i));
+				br = new BufferedReader(fr);
+			
+			//売上ファイルの1行目には支店コード、2行目には売上金額が入っています→.readLineは1行ずつ読み出し、2回で1行目と2行目で出力したい
+				String branchCode = br.readLine(); 
+				String saleLine = br.readLine();
+			//売上ファイルから読み込んだ売上金額をMapに加算していくために、型の変換を行います。
+			//※詳細は後述で説明
+			long fileSale = Long.parseLong(saleLine); 
+			
+			//読み込んだ売上⾦額を加算します。
+			//※詳細は後述で説明
+			//Long saleAmount = 売上金額を入れたMap.get(支店コード) + long に変換した売上金額;
+			Long saleAmount = branchSales.get(branchCode)+ fileSale;
+
+			//加算した売上金額をMapに追加します。
+			branchSales.put(branchCode, saleAmount);
+			  } catch (IOException e) {
+			        // 例外処理（エラーが発生した場合の対応）
+			        System.out.println("UNKNOWN_ERROR");
+			        return; 
+			    } finally {
+			        // ファイルを閉じます。
+			        if (br != null) {
+			            try {
+			                br.close();
+			            } catch (IOException e) {
+			                System.out.println("UNKNOWN_ERROR");
+			            }
+			        }
+			    }  
+		}
 
 		// 支店別集計ファイル書き込み処理
 		if(!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
@@ -69,22 +119,13 @@ public class CalculateSales {
 				// ※ここの読み込み処理を変更してください。(処理内容1-2)
 				//書き込み
 			    String[] items = line.split(",");
-
 			    //Mapに追加する2つの情報をputの引数として指定します。
                 //支店定義フォルダの支店コード、支店名の文字列分割、保持
-			    branchNames.put("001","札幌支店");
-			    branchNames.put("002","仙台支店");
-			    branchNames.put("003","東京支店");
-			    branchNames.put("004","名古屋支店");
-			    branchNames.put("005","大阪支店");
-
+          
+			    branchNames.put(items[0], items[1]);
 			    //支店コードと売上金額を保持する
-			    branchSales.put("001",0L);
-			    branchSales.put("002",0L);
-			    branchSales.put("003",0L);
-			    branchSales.put("004",0L);
-			    branchSales.put("005",0L);
 
+			    branchSales.put(items[0], 0L);
 				System.out.println(line);
 			}
 
